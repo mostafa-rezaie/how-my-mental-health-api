f8fadf2d674934e15ce8e368ae87a999fd534dcb
mostafa-rezaie <mostafa.rezaie@outlook.com>
2022-07-02 20:42:34 +0430

    Create UserProfile model

--- himml_rest_api/settings.py
+++ himml_rest_api/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -27,10 +26,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'profile_api',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -67,8 +67,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'himml_rest_api.wsgi.application'
+AUTH_USER_MODEL = 'profile_api.UserProfile'
 
+WSGI_APPLICATION = 'himml_rest_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
@@ -79,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -99,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -110,7 +109,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- profile_api/models.py
+++ profile_api/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, username, gender, password=None):
+        if not email:
+            raise ValueError('users must have email')
+        if not username:
+            raise ValueError('users must have username')
+        if not gender:
+            raise ValueError('users must have gender specific')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            gender=gender,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, gender, password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            gender=gender,
+            password=password
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    GENDER_MALE = 'M'
+    GENDER_FEMALE = 'F'
+    GENDER_UNKNOWN = 'U'
+    GENDER_CHOICE = ((GENDER_MALE, 'Male'), (GENDER_FEMALE, 'Female'), (GENDER_UNKNOWN, 'Unknown'))
+
+    email = models.CharField(max_length=64, verbose_name='email', unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    date_joined = models.DateTimeField(verbose_name='date-joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last-login', auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICE, null=False)
+    date_of_birth = models.DateField(verbose_name='date-of-birth', null=True, blank=True)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'gender']
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.username
