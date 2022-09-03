fe10a0f6a1de4ed32473c97c4dd4521449c9b1bd
mostafa-rezaie <mostafa.rezaie@outlook.com>
2022-09-04 01:43:25 +0430

    add created field to admin panel

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Questionnaires, Question, Answers, Results
 
+
+class ResultsAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+
 # Register your models here.
 admin.site.register(Questionnaires)
 admin.site.register(Question)
 admin.site.register(Answers)
-admin.site.register(Results)
+admin.site.register(Results, ResultsAdmin)
