7c5c10b285563eb8ec435f37a090369c79433c51
mostafa-rezaie <mostafa.rezaie@outlook.com>
2022-09-04 10:58:19 +0430

    add reset password

--- himml_rest_api/settings.py
+++ himml_rest_api/settings.py
@@ -133,3 +133,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'rezaie.mostafa1998@gmail.com'
+EMAIL_HOST_PASSWORD = 'sinlozmbctrzbdli'
--- himml_rest_api/urls.py
+++ himml_rest_api/urls.py
@@ -15,10 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', include('profile_api.urls')),
-    path('tests-api/', include('questionnaire.urls'))
+    path('tests-api/', include('questionnaire.urls')),
+    # reset password urls
+    path('reset-password/', auth_views.PasswordResetView.as_view(
+        template_name='./profile_api/password_reset.html'),
+         name='reset_password'),
+    path('reset-password_sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name='./profile_api/password_reset_sent.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset-password-compelete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='./profile_api/password_reset_done.html'),
+         name='password_reset_complete'),
 
 ]
