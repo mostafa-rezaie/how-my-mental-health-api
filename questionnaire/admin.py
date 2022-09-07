fa6db030b835a90409e853e05faf40f512702681
mostafa-rezaie <mostafa.rezaie@outlook.com>
2022-09-07 12:51:27 +0430

    add id field to Questionnaire admin panel

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -6,8 +6,12 @@ class ResultsAdmin(admin.ModelAdmin):
     readonly_fields = ('created',)
 
 
+class QuestionnaireAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+
 # Register your models here.
-admin.site.register(Questionnaires)
+admin.site.register(Questionnaires,QuestionnaireAdmin)
 admin.site.register(Question)
 admin.site.register(Answers)
 admin.site.register(Results, ResultsAdmin)
