553c6dd11823ec7633f5e06def9afeaed21efbb9
mostafa-rezaie <mostafa.rezaie@outlook.com>
2022-09-10 14:14:59 +0430

    submit test results for bfp

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -63,14 +63,47 @@ class Answer(CreateAPIView):
             question = Question.objects.filter(questionnaire__name=questionnaire_name).get(
                 qid=qid)
             Answers.objects.create(user=request.user, question=question, answer=answer_choice)
-        score = sum_ans * 3.1
-        print('>>>>>>>>>>>1')
+        if questionnaire_name.lower() == 'ghq':
+            score = sum_ans - 12
+            results = 'Not mention'
+        elif questionnaire_name.lower() == 'bfp':
+            ans_choice = []
+            for answer in answers:
+                ans_choice.append(int(answer['answer']))
+            extr_factor = 20 + ans_choice[0] - ans_choice[5] + ans_choice[10] - ans_choice[15] + ans_choice[20] - \
+                          ans_choice[25] + ans_choice[30] - ans_choice[35] + ans_choice[40] - ans_choice[45]
+
+            agree_factor = 14 - ans_choice[1] + ans_choice[6] - ans_choice[11] + ans_choice[16] - ans_choice[21] + \
+                           ans_choice[26] - ans_choice[31] + ans_choice[36] + ans_choice[41] + ans_choice[46]
+
+            cons_factor = 14 + ans_choice[2] - ans_choice[7] + ans_choice[12] - ans_choice[17] + ans_choice[22] - \
+                          ans_choice[27] + ans_choice[32] - ans_choice[37] + ans_choice[42] + ans_choice[47]
+
+            neu_factor = 38 - ans_choice[3] + ans_choice[8] - ans_choice[13] + ans_choice[18] - ans_choice[23] - \
+                         ans_choice[28] - ans_choice[33] - ans_choice[38] - ans_choice[43] - ans_choice[48]
+
+            open_factor = 8 + ans_choice[4] - ans_choice[9] + ans_choice[14] - ans_choice[19] + ans_choice[24] - \
+                          ans_choice[29] + ans_choice[34] + ans_choice[39] + ans_choice[44] + ans_choice[49]
+
+            # print('>>>>>>', open_factor)
+            # print('>>>>>>', extr_factor)
+            # print('>>>>>>', agree_factor)
+            # print('>>>>>>', cons_factor)
+            # print('>>>>>>', neu_factor)
+
+            score = 12
+            results = \
+                'Openness: ' + str(open_factor) + ' Conscientiousness: ' + str(
+                    cons_factor) + ' Extroversion: ' + str(extr_factor) + ' Agreeableness:' + str(
+                    agree_factor) + ' Neuroticism: ' + str(neu_factor)
+        else:
+            score = -1
         Results.objects.create(user=request.user,
                                num_of_question_answered=answers_count,
                                duration=320,
                                score=score,
-                               questionnaire=questionnaire)
-        print('>>>>>>>>>>>2')
+                               questionnaire=questionnaire,
+                               description=results)
         return Response({'message': 'answers submitted successfully'})
 
 
